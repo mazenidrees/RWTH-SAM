a42b37106ed295c7fa8c98d30c9155f0cc2c11f0
Mazen Adris <qch8nhancb23czuk@students.rwth-aachen.de>
2023-08-11 05:34:12 +0200

    Reader function reads a json file returns a points layer with segmentation profile as metadata

--- src/rwth_sam/_reader.py
+++ src/rwth_sam/_reader.py
@@ -1,72 +1,32 @@
-"""
-This module is an example of a barebones numpy reader plugin for napari.
-
-It implements the Reader specification, but your plugin may choose to
-implement multiple readers or even other plugin contributions. see:
-https://napari.org/stable/plugins/guides.html?#readers
-"""
+import json
 import numpy as np
-
-
+# TODO: check json structure before reading/returning data
 def napari_get_reader(path):
-    """A basic implementation of a Reader contribution.
-
-    Parameters
-    ----------
-    path : str or list of str
-        Path to file, or list of paths.
-
-    Returns
-    -------
-    function or None
-        If the path is a recognized format, return a function that accepts the
-        same path or list of paths, and returns a list of layer data tuples.
-    """
-    if isinstance(path, list):
-        # reader plugins may be handed single path, or a list of paths.
-        # if it is a list, it is assumed to be an image stack...
-        # so we are only going to look at the first file.
-        path = path[0]
-
-    # if we know we cannot read the file, we immediately return None.
-    if not path.endswith(".npy"):
+    if not path.endswith(".json"):
         return None
+    
+    return json_reader_function
 
-    # otherwise we return the *function* that can read ``path``.
-    return reader_function
 
-
-def reader_function(path):
-    """Take a path or list of paths and return a list of LayerData tuples.
-
-    Readers are expected to return data as a list of tuples, where each tuple
-    is (data, [add_kwargs, [layer_type]]), "add_kwargs" and "layer_type" are
-    both optional.
+def json_reader_function(path):
+    """Take a path to a JSON file, read the metadata, and return a Points layer with zero points and the extracted metadata.
 
     Parameters
     ----------
-    path : str or list of str
-        Path to file, or list of paths.
+    path : str
+        Path to JSON file.
 
     Returns
     -------
     layer_data : list of tuples
-        A list of LayerData tuples where each tuple in the list contains
-        (data, metadata, layer_type), where data is a numpy array, metadata is
-        a dict of keyword arguments for the corresponding viewer.add_* method
-        in napari, and layer_type is a lower-case string naming the type of
-        layer. Both "meta", and "layer_type" are optional. napari will
-        default to layer_type=="image" if not provided
+        A list of LayerData tuples where each tuple contains (data, metadata, layer_type). 
     """
-    # handle both a string and a list of strings
-    paths = [path] if isinstance(path, str) else path
-    # load all files into array
-    arrays = [np.load(_path) for _path in paths]
-    # stack arrays into single array
-    data = np.squeeze(np.stack(arrays))
+    # Load and parse the JSON file
+    with open(path, 'r') as file:
+        metadata = json.load(file)
+    # Create an empty Points layer data to hold the metadata
+    data = np.empty((0, 2))  # Zero points in a 2D space
+    add_kwargs = {"metadata": metadata}
+    layer_type = "points"
 
-    # optional kwargs for the corresponding viewer.add_* method
-    add_kwargs = {}
-
-    layer_type = "image"  # optional, default is "image"
     return [(data, add_kwargs, layer_type)]
